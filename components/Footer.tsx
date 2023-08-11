7937e64f19763ac66a1d0d1fb428735d308ea1ce
hukuryo <ryohei.1998.05.09@gmail.com>
2023-08-11 21:21:55 +0900

    レイアウト修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,25 +2,55 @@ import React from 'react';
 
 export function Footer() {
     return (
-        <footer className="bg-gray-900 text-white py-6 mt-60">
-            <div className="container mx-auto flex justify-center items-center">
-                <div className="text-center">
-                    <p className="text-lg font-semibold">Stay Connected</p>
-                    <div className="flex justify-center space-x-4 mt-4">
-                        <a href="#" className="text-gray-400 hover:text-gray-300 transition duration-300">
-                            <i className="fab fa-facebook-f text-xl"></i>
-                        </a>
-                        <a href="#" className="text-gray-400 hover:text-gray-300 transition duration-300">
-                            <i className="fab fa-twitter text-xl"></i>
-                        </a>
-                        <a href="#" className="text-gray-400 hover:text-gray-300 transition duration-300">
-                            <i className="fab fa-instagram text-xl"></i>
-                        </a>
-                        <a href="#" className="text-gray-400 hover:text-gray-300 transition duration-300">
-                            <i className="fab fa-linkedin-in text-xl"></i>
-                        </a>
+        <footer className="py-12 mt-48 bg-gray-50 dark:bg-gray-800">
+            <div className="w-full px-32 mx-auto max-w-8xl">
+                <div className="grid gap-12 md:grid-cols-3 sm:gap-24">
+                    <div>
+                        <h3 className="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">発信媒体</h3>
+                        <ul>
+                            <li className="mb-4">
+                                <a href="https://github.com/themesberg/flowbite/discussions" rel="noreferrer nofollow" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Github</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://github.com/themesberg/flowbite/discussions" rel="noreferrer nofollow" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Twitter</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">オススメの記事</h3>
+                        <ul>
+                            <li className="mb-4">
+                                <a href="https://discord.gg/4eeurUVvTy" rel="noreferrer nofollow" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Discord</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://github.com/themesberg/flowbite" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Github</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://twitter.com/zoltanszogyenyi" rel="noreferrer nofollow" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Twitter</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">カテゴリー</h3>
+                        <ul>
+                            <li className="mb-4">
+                                <a href="https://flowbite.com/license/" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">License (EULA)</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://flowbite.com/privacy-policy/" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Privacy Policy</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://flowbite.com/terms-and-conditions/" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Terms &amp; Conditions</a>
+                            </li>
+                            <li className="mb-4">
+                                <a href="https://flowbite.com/brand/" className="font-medium text-gray-600 dark:text-gray-400 dark:hover:text-white hover:underline">Brand guideline</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
+                <hr className='mt-10 mb-10'/>
+                <span className="block text-center text-gray-600 dark:text-gray-400 font-">© 2023 Hukuryo-no-Blog
+                </span>
             </div>
         </footer>
     );
--- components/ScrollUp.tsx
+++ components/ScrollUp.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 import { animateScroll as scroll } from "react-scroll";
 
 export function ScrollUp() {
+    // スクロールアップボタンを表示、非表示させるためのState
+    const [isVisible, setIsVisible] = useState(false);
+
+    // スクロールしている高さを取得して、stateの値を更新する処理
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) { // ここで表示するスクロール位置を調整
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+        
+        window.addEventListener('scroll', toggleVisibility);
+
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
     const scrollToTop = () => {
         scroll.scrollToTop();
     };
     
     return (
-        <button className='fixed bottom-6 right-6 z-50 bg-gray-500 text-white px-4 py-3 rounded-full cursor-pointer transition-opacity' onClick={scrollToTop}>
-            <FontAwesomeIcon icon={faChevronUp} />
-        </button>
+        <div className="fixed bottom-6 right-6 z-50">
+            <button className={`fixed bottom-6 right-6 z-50 bg-gray-500 text-white px-4 py-3 rounded-full cursor-pointer transition-opacity ${isVisible ? 'opacity-100' : 'opacity-0'}`} onClick={scrollToTop}>
+                <FontAwesomeIcon icon={faChevronUp} />
+            </button>
+        </div>
     )
 }
