67581ce01ddd4c1cabae561443d08ffb53513335
hukuryo <ryohei.1998.05.09@gmail.com>
2023-08-24 21:13:07 +0900

    レイアウト修正

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -9,20 +9,20 @@ import { ArticleProps } from "../types/article";
 const ArticleList: React.FC<ArticleProps> = ({ articles, pass }) => {
     
     return (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
             {articles.map((article) => (
                 <li key={article.id}>
                 <Link href={`/${ pass }/${article.id}`} legacyBehavior>
                     <a>
                         <article 
-                            className="flex-start mx-auto flex rounded-md border-2 border-stone-200 bg-stone-50 p-4 shadow-md duration-200 transition-transform transform hover:scale-105 hover:opacity-80 md:p-4"
+                            className="flex-start mx-auto flex rounded-md border-stone-200 bg-stone-50 p-4 shadow-md duration-200 transition-transform transform hover:scale-105 hover:opacity-80 md:p-4"
                         >
                         <Image
                             src={article.imageUrl.url}
                             alt="見出し画像"
-                            width={60}
+                            width={80}
                             height={60}
-                            className="my-auto ml-0 mr-2 max-w-md rounded-md border-stone-300 bg-stone-200 shadow-md dark:bg-stone-100 dark:opacity-80 md:h-20 md:w-20"
+                            className="my-auto ml-0 mr-4 max-w-full rounded-md border-stone-300 bg-stone-200 shadow-md dark:bg-stone-100 dark:opacity-80 md:h-20 md:w-20"
                         />
                             <div>
                                 <h3 className="text-sm text-stone-800 line-clamp-3 dark:text-stone-200 md:text-base">{article.title}</h3>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: LayoutProps) {
         <>
         <main className="sm:px-10 pb-48 bg-indigo-50">
             <div className="flex pt-10 flex-row-reverse">
-            <div className="p-10 sm:mr-3 sm:ml-5 mb-10 w-full">
+            <div className="p-10 md:mr-3 md:ml-5 mb-10 w-full">
                 {children}
             </div>
             </div>
